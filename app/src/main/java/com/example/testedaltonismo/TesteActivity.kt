b60a52cb15519a47c194a1d9a704baade6322452
Bia Chacon <anabheatrizchacon@outlook.com>
2019-08-14 15:28:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/testedaltonismo/TesteActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testedaltonismo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TesteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teste)
+    }
+}

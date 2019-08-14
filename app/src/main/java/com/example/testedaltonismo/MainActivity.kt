d24201dc2bbb0752e767c3ae8fe0c74c297ca4b9
Bia Chacon <anabheatrizchacon@outlook.com>
2019-08-14 17:01:39 -0300

    .

--- app/src/main/java/com/example/testedaltonismo/MainActivity.kt
+++ app/src/main/java/com/example/testedaltonismo/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.testedaltonismo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var resultT1 = 0
+    var resultT2 = 0
+    var resultT3 = 0
+
+    var resultFinal = "Faça o teste"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resposta1TextView.text = ("$resultT1")
+        resposta2TextView.text = ("$resultT2")
+        resposta3TextView.text = ("$resultT3")
+
+        resultadoTextView.text = ("$resultFinal")
+
     }
+
+    fun  clique(v:View){
+
+    }
+
 }
--- app/src/main/java/com/example/testedaltonismo/TesteActivity.kt
+++ app/src/main/java/com/example/testedaltonismo/TesteActivity.kt
@@ -1,12 +1,25 @@
 package com.example.testedaltonismo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_teste.*
 
 class TesteActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teste)
+
+        var param = intent.extras
+        var id = param?.getInt("ID")
+
+        if(id == 1)
+            imageView.setImageResource(R.drawable.im2)
+        if(id == 2)
+            imageView.setImageResource(R.drawable.im15)
+        if(id == 2)
+            imageView.setImageResource(R.drawable.im74)
+
     }
 }
